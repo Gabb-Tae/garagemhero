96c9bf50124e1998f35e50c3e0ae66f9b9e7fc82
Gabriel <gabbmaia2004@gmail.com>
2022-08-18 15:15:02 -0300

    xomit

--- garagemhero/settings.py
+++ garagemhero/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-uvvfp8rmqz988@06&#*wwnv2=38rp1t4pme@7ali775xun^$m)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
